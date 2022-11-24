4e81ec681cb5f61261941a214da975f88d48743a
FERRALI-Mario <you@example.com>
2022-11-24 16:47:20 +0100

    :sparkles: ButtonComponent

--- vitrine/pages/login.tsx
+++ vitrine/pages/login.tsx
@@ -2,13 +2,16 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import FooterComponent from "../components/FooterComponent";
 import HeadComponent from "../components/HeadComponent";
+import ButtonComponent from "../components/ButtonComponent";
 
 const Login: NextPage = () => {
   const router = useRouter();
   return (
     <div>
       <HeadComponent />
-      <main></main>
+      <main>
+          <ButtonComponent />
+      </main>
       <FooterComponent />
     </div>
   );
