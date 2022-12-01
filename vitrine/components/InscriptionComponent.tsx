e716db31e9e12f29f21683390d95dc3a602465bd
tom-guastapaglia <tomguasta@yahoo.fr>
2022-12-01 11:19:42 +0100

    web version

--- vitrine/components/InscriptionComponent.tsx
+++ vitrine/components/InscriptionComponent.tsx
@@ -10,15 +10,17 @@ return (
                 <RadioComponent label="une entreprise" type="radio" name="type" />
                 <RadioComponent label="un particulier" type="radio" name="type" />
             </div>
-            <InputTextComponent label="Nom" />
-            <InputTextComponent label="Prénom" />
-            <InputTextComponent label="E-mail" />
-            <InputTextComponent label="Numéro de téléphone" />
-            <SelectComponent label="Nationalité" >
-                <option value="france">France</option>
-            </SelectComponent>  
+            <div className="grid-form">
+                <InputTextComponent label="Nom"/>
+                <InputTextComponent label="Prénom" />
+                <InputTextComponent label="E-mail" />
+                <InputTextComponent label="Numéro de téléphone" />
+                <SelectComponent label="Nationalité" >
+                    <option value="france">France</option>
+                </SelectComponent>
+            </div>
             <CheckboxComponent label="j'atteste que je possède un permis de conduite valide." type="checkbox" />
-            <ButtonComponent label="Demander mon inscription" />
+            <ButtonComponent label="Demander mon inscription"/>
         </div>
     </div>
 );
